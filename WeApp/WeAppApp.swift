c2f6b38adc0dcc360ae75413468ed61cd2a3b2f6
Daniil Liplenko <prod408@yandex.ru>
2025-02-02 13:25:04 +0400

    Initial Commit

---
+++ WeApp/WeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeAppApp.swift
+//  WeApp
+//
+//  Created by Даниил Липленко on 02.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
